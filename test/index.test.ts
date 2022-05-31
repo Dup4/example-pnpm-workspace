a26d11754e34a25fef9af04c5a85d8a78b534019
Dup4 <lyuzhi.pan@gmail.com>
2022-05-31 11:21:14 +0800

    chore: remove extra src

--- test/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from "vitest";
-import { one, two } from "../src/index";
-
-describe("should", () => {
-  it("exported", () => {
-    expect(one).toEqual(1);
-    expect(two).toEqual(2);
-  });
-});
